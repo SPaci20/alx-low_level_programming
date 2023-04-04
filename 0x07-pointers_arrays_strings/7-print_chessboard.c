ea765032d099e5661ad8c753e1509cbec136532c
SPaci20 <spaci20@gmail.com>
2023-04-04 07:55:14 -0700

    a functions that prints chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,13 +6,13 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int x;
-	int y;
+	int i;
+	int j;
 
-	for (x = 0; x < 8; x++)
+	for (i = 0; i < 8; i++)
 	{
-		for (y = 0; y < 8; y++)
-			_putchar(a[x][y]);
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
 		_putchar('\n');
 	}
 }
