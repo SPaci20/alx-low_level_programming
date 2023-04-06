0535a5fe8ab3d8de22e627b2c6fbdf9d8cfe7ea4
SPaci20 <spaci20@gmail.com>
2023-04-06 08:17:52 -0700

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _is_prime_recursion - checks if a number is prime using recursion
- *is_prime_number - checks if a number is prime
+ * is_prime_number - checks if a number is prime
  * @n: the number to check
  * @divisor: the current divisor being tested for primality
  * Return: 1 if n is prime, 0 otherwise
