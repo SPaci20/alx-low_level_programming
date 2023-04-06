3bc7bf386bd02e1dbe7ec9e4f6d4e1f18fc16cec
SPaci20 <spaci20@gmail.com>
2023-04-06 06:46:25 -0700

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _pow_recursion - Return the value of x raised by the power of y
- *@x : any given integer that will be powered by y
- @y: any given integer that will be used as power
+ *@x : value that will be powered
+ * @y: any given integer that will be used as power
  * Return: Value of x powered by y
  */
 int _pow_recursion(int x, int y)
