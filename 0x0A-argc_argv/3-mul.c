f6f2fefb664e0f07877bde9b2d59a2b7b8a77660
SPaci20 <spaci20@gmail.com>
2023-04-09 04:17:52 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point. Multiplies two numbers and prints the result.
+ * @argc: The number of arguments.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: 0 if successful, 1 if an error occurred.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+
+	return (0);
+}
