6e15382ce184817085701f7b288b6c5ff584b594
SPaci20 <spaci20@gmail.com>
2023-04-09 04:09:33 -0700

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point. Prints all arguments passed to the program.
+ * @argc: The number of arguments.
+ * @argv: An array of strings containing the arguments.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
