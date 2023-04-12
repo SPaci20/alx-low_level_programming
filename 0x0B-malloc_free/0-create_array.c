cafe41552a99d38bb8e197a39d1cbded7437afff
SPaci20 <spaci20@gmail.com>
2023-04-12 04:54:39 -0700

    TAsk 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
-#include "main.h"		
+#include "main.h"
 #include <stdlib.h>
 /**
- * create_array - create array of size size and assign char c		
+ * create_array - create array of size size and assign char c
  * @size: size of array
  * @c: char to assign
  * Description: creat array of size size and assign char c
@@ -20,5 +20,5 @@ char *create_array(unsigned int size, char c)
 	for (i = 0; i < size; i++)
 		str[i] = c;
 
-	return (str);	
+	return (str);
 }
