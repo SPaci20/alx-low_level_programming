497baf38918eae1580f29e5baecebe12a6a216d2
SPaci20 <spaci20@gmail.com>
2023-04-18 09:47:51 -0700

    Task 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,24 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
-struct dog
+/**
+ * struct dog - struture of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+typedef struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
-};
+	char *owner;
+
+} dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /* #ifndef DOG_H */
