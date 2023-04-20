d03a3dde1ea4aa148c4c2fe96be2500bc749b5e5
SPaci20 <spaci20@gmail.com>
2023-04-20 01:50:59 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,9 +15,9 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(ap, n);
 	sum = 0;
-	for (i=0; i < n ; i++)
-		sum += va_arg (ap, int);
-	va_end (ap);
+	for (i = 0; i < n ; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
 
-		return sum;
+		return (sum);
 }
