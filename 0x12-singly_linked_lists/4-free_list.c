e9adcd2da790d72b5ca15b3deccd10afa00b272b
SPaci20 <spaci20@gmail.com>
2023-04-27 19:54:34 -0700

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: Pointer to the head of the list
+ *
+ * Description: This function frees a linked list by iterating through it and
+ *              freeing the memory allocated for each node and its string.
+ */
+
+void free_list(list_t *head)
+{
+    list_t *temp;
+
+    while (head)
+    {
+        temp = head->next;
+        free(head->str);
+        free(head);
+        head = temp;
+    }
+}
