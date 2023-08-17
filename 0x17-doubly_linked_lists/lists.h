4858471d5cb0e7326151c98eeabdf0bfe7b4c54f
SPaci20 <spaci20@gmail.com>
2023-08-17 07:02:52 -0700

    Task7

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -25,5 +25,7 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 /* Function that returns the sum of all the data (n) of a linked list */
 int sum_dlistint(dlistint_t *head);
+/* Function that inserts a new node at a given position.*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
 #endif /* LISTS_H */
