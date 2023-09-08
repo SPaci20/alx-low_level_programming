134c45ae9c0c70d493e2e69f5349caaa869909e3
SPaci20 <spaci20@gmail.com>
2023-09-08 00:40:44 -0700

    Tak 5 Upd.

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,32 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - Retrieves a value associated with a key in a hash table.
- * @ht: The hash table.
- * @key: The key to look for.
- *
- * Return: The value associated with the key, or NULL if the key couldn't be found.
+ * hash_table_print - Prints a hash table.
+ * @ht: The hash table to print.
  */
-char *hash_table_get(const hash_table_t *ht, const char *key)
+void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int index;
-    hash_node_t *current_node;
+	unsigned long int i;
+	hash_node_t *node;
+	int first = 1;  /* Flag to check if it's the first key-value pair */
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (NULL);
+	if (ht == NULL)
+		return;
 
-    /* Calculate the index where the key should be stored */
-    index = key_index((unsigned char *)key, ht->size);
-
-    /* Search for the key in the linked list at the calculated index */
-    current_node = ht->array[index];
-    while (current_node)
-    {
-        if (strcmp(current_node->key, key) == 0)
-            return (current_node->value);
-        current_node = current_node->next;
-    }
-
-    /* Key not found in the hash table */
-    return (NULL);
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			if (first)
+				first = 0;
+			else
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+		}
+	}
+	printf("}\n");
 }
